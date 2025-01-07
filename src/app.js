73a76427991e15fd914c20a44fc07936fac46245
Woodsii <beanmanwoods@gmail.com>
2025-01-07 16:13:40 -0600

    Add echo example code

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import mf from "mineflayer"
 
-const options = {
-    host: 'localhost', 
-    port: 25565 
-  }
+const bot = mf.createBot({
+  host: '192.168.0.13', // Minecraft server IP
+  port: 25565,       // Port number (default is 25565)
+  username: 'Bot',   // Bot username
+  auth: 'offline'    // Offline mode authentication
+})
 
-const bot = mf.createBot();
+bot.on('chat', (username, message) => {
+  if (username === bot.username) return
+  bot.chat(message)
+})
+
+// Log errors and kick reasons:
+bot.on('kicked', console.log)
+bot.on('error', console.log)
