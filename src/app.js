fab28fbae337635dec436d2d22148364f1d8163c
Woodsii <beanmanwoods@gmail.com>
2025-01-10 17:25:32 -0600

    Enable bot to move

--- src/app.js
+++ src/app.js
@@ -1,27 +1,44 @@
-import mf from "mineflayer"
-import { mineflayer as mineflayerViewer } from 'prismarine-viewer';
+import mineflayer from "mineflayer"
 
-const bot = mf.createBot({
-  host: 'x', // Minecraft server IP, replace x
+const bot = mineflayer.createBot({
+  host: '192.168.0.13', // Minecraft server IP, replace x
   port: 25565,       // Port number (default is 25565)
   username: 'Bot',   // Bot username
   auth: 'offline',   // Offline mode authentication
   version: '1.20.1'
 })
 
-bot.on('chat', (username, message) => {
-  if (username === bot.username) return
-  bot.chat(message)
-})
-
-bot.once('spawn', () => {
-  mineflayerViewer(bot, { 
-    port: 3007, 
-    firstPerson: true,
-    version: '1.20.1'
-  }); // port is the viewer port, not the server port
-});
-
 // Log errors and kick reasons:
 bot.on('kicked', console.log)
 bot.on('error', console.log)
+
+// Controlled via chat
+bot.on('chat', async (username, message) => {
+  if (username === bot.username) return
+
+  let degrees = 0;
+  switch (message) {
+    case 'MOVE':
+      bot.setControlState('forward', true)
+
+      degrees = 0;
+      while (degrees <= 360){
+        await bot.look(0.0174533, 0, false) // change yaw by 1 degree
+        degrees += 1
+      }
+      
+      break;
+
+    case 'STOP':
+      bot.setControlState('forward', false)
+      break;
+
+    case 'TURN':
+      degrees = 0;
+      while (degrees <= 360){
+        await bot.look(0.0174533, 0, false) // change yaw by 1 degree
+        degrees += 1
+      }
+      break;
+  }
+});
